087b6e69288d6186c4c56af5f5cee6e1e67f6235
jzhupan <jzhupan@gmail.com>
2023-04-01 20:38:09 -0400

    Attempted to solve level 3 DOTS game

--- starter/level-three.js
+++ starter/level-three.js
@@ -6,10 +6,44 @@
 let score = 0
 let scoreDisplay = document.querySelector('.js-score')
 let levelWinner = document.querySelector('.level-winner')
+let ballArray= document.querySelectorAll('ball, .js-ball')
+let ballsArena= document.querySelectorAll('.js-ball')
 
-let smallBall = document.querySelector('.js-ball').size = "20"
-let bigBall = document.querySelector('.js-ball').size = "5"
-let mediumBall = document.querySelector('.js-ball').size = "10"
+console.log(ballsArena)
 
+/*
+let smallBall = document.querySelector('.js-ball').nextElementSibling.nextElementSibling
+let bigBall = document.querySelector('.js-ball').nextElementSibling.nextElementSibling;
+let mediumBall = document.querySelector('.js-ball').nextElementSibling;
+
+console.log(smallBall)
+console.log(bigBall)
+console.log(mediumBall)
+*/
+
+// ballsArena.forEach((element, index) => {
+//     element.setAttribute('data-increment', index)
+
+// })
+
+let forEachBall = ballsArena.forEach(element => console.log(element.getAttribute('data-increment')))
+
+// for (let i = 0; i < ballsArena.length; i++) {
+//     ballsArena[i].addEventListener("click",groupClick(i))
+//  }
+
+//console.log(ballArray)
+
+function groupClick(){
+    forEachBall.addEventListener('click', ()=>{
+        score  += forEachBall.getAttribute('data-increment')
+        score = scoreDisplay.innerText
+        console.log('im working')
+        if(score >= 100){
+            levelWinner.style.opacity = 1
+        }
+    })
+    console.log(scoreDisplay)
+}
 
 
